69ed98ee83b159991001f09cc4db2845b6cba0f8
jairobernal3000 <jairobernal3000@gmail.com>
2022-03-02 22:05:58 -0500

    Se agrega log para mosgrar el puerto del servicio

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Bootstrap');
+  const port = 3000;
+  
+  await app.listen(port);
+  logger.log(`Server is running at ${await app.getUrl()}`);
 }
 bootstrap();
